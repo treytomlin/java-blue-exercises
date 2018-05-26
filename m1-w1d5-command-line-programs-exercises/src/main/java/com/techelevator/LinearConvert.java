2a2a6a1e1598aa7be12b3fd49bde79477559dd44
ttomlin <treytomlin91@gmail.com>
2018-05-25 20:02:26 -0400

    Linear Convert Complete

--- m1-w1d5-command-line-programs-exercises/src/main/java/com/techelevator/LinearConvert.java
+++ m1-w1d5-command-line-programs-exercises/src/main/java/com/techelevator/LinearConvert.java
@@ -1,5 +1,7 @@
 package com.techelevator;
 
+import java.util.Scanner;
+
 /*
  The foot to meter conversion formula is:
  	m = f * 0.3048
@@ -19,7 +21,42 @@ package com.techelevator;
 public class LinearConvert {
 
 	public static void main(String[] args) {
+		while (true) {
+			Scanner in = new Scanner(System.in);
+			System.out.println("Select your Unit of measurement!");
+			System.out.print("Enter (M)eters or (F)eet || Enter (Q) to quit) >>>");
+			String userUnit = in.nextLine().toUpperCase();
+			System.out.println("And how many?");
+			System.out.println("Give me an Integer please! >>");
 
-	}
+			String userInt = in.nextLine().toUpperCase();
+			if(userInt.equals("Q")) {
+				break;
+			}
+				
+				switch(userUnit){
+				case "M":
+					String meterConversion = userInt;
+					double result = Integer.parseInt(meterConversion) * 3.28 ;
+					System.out.println(userInt + " Meters is " + result + " feet!");
+					break;
+				case "F":
+					String feetConversion = userInt;
+					double result2 = Integer.parseInt(feetConversion) * 0.3048 ;
+					System.out.println(userInt + " Feet is " + result2 + " Meters!");
+					break;
+				
+
+
+			}
+	
+				
+
+		
+			
+					
+			}
+	
+		}
 
 }
