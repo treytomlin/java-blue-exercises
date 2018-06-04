8a4586d213861be7aff5fd508851d891a8289c7c
Andrew Frank <andrew@techelevator.com>
2018-06-04 19:59:56 -0400

    Resolving merge conflicts in FruitTree.java.

--- m1-w3d1-oop-with-encapsulation-exercises/src/main/java/com/techelevator/FruitTree.java
+++ m1-w3d1-oop-with-encapsulation-exercises/src/main/java/com/techelevator/FruitTree.java
@@ -16,31 +16,12 @@ public class FruitTree {
 		return typeOfFruit;
 	}
 
-<<<<<<< HEAD
-=======
-	private String typeOfFruit;
-	private int piecesOfFruitLeft;
-	
-	public FruitTree(String typeOfFruit, int startingPiecesOfFruit) {
-		this.typeOfFruit = typeOfFruit;
-		this.piecesOfFruitLeft = startingPiecesOfFruit;
-	}
-
-	public String getTypeOfFruit() {
-		return typeOfFruit;
-	}
-
->>>>>>> a6158c3fcb93538c8e077359be22942d5feb4dad
 	public int getPiecesOfFruitLeft() {
 		return piecesOfFruitLeft;
 	}
 	
 	public boolean pickFruit(int numberOfPiecesToRemove) {
-<<<<<<< HEAD
-		if(numberOfPiecesToRemove > piecesOfFruitLeft) {
-=======
 		if (numberOfPiecesToRemove > this.piecesOfFruitLeft) {
->>>>>>> a6158c3fcb93538c8e077359be22942d5feb4dad
 			return false;
 		} else {
 			this.piecesOfFruitLeft -= numberOfPiecesToRemove;
