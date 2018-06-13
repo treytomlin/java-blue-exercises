1c6d9bef8c4dd821d57166586eff0f3ef5a2ee29
ttomlin <treytomlin91@gmail.com>
2018-06-13 18:42:40 -0400

    wordSearch completed

--- m1-w4d2-file-io-part1-exercises/src/main/java/com/techelevator/WordSearch.java
+++ m1-w4d2-file-io-part1-exercises/src/main/java/com/techelevator/WordSearch.java
@@ -4,42 +4,25 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.NoSuchElementException;
 import java.util.Scanner;
+/***
+ * 
+ * @author ttomlin
+ *
+ */
 
 
 public class WordSearch {
 
 	public static void main(String[] args) throws FileNotFoundException, NoSuchElementException {
+		
 		File scanFile = getInputFileFromUser();
 		String wordToSearch = getSearchWordFromUser();
-		
-		try(Scanner fileReader = new Scanner(scanFile)){
-		int counter = 0;
-	
-			while(fileReader.hasNextLine()) {
-				counter++;
-				String line = fileReader.nextLine();
-				if(line.contains(wordToSearch)) {
-					System.out.println(counter + ")" +line);
-					
-				
-			} else { 
-				 line = fileReader.nextLine();
-				 if(line.contains(wordToSearch)) {
-					 System.out.println(counter + ")" + line);
-				 }
-			}
-			}
-	}
-			}
+		askUserIfCaseSensitive(scanFile, wordToSearch);
+}
 
-		
-		
-		
-		
-	
-	
-
+@SuppressWarnings("resource")
 public static File getInputFileFromUser() {
+	
 		Scanner userInput = new Scanner(System.in);
 		System.out.print("Please enter the path of the file to scan");
 		String path = userInput.nextLine();
@@ -53,19 +36,69 @@ public static File getInputFileFromUser() {
 			System.out.println(path+" is not a file");
 			System.exit(1); // Ends the program
 		}
-		//userInput.close();
+		
 		return inputFile;
 	}
 
 	
 
+@SuppressWarnings("resource")
 public static String getSearchWordFromUser() {
 	Scanner searchWordReader = new Scanner(System.in);
 
 	System.out.println("Please enter the word to search");
 	String searchWord = searchWordReader.nextLine();
-	//searchWordReader.close();
 	return searchWord;
+}
+
+
+public static String askUserIfCaseSensitive(File scanFile, String wordToSearch) throws FileNotFoundException {
+		
+		Scanner caseSensitiveScanner = new Scanner(System.in);
+		System.out.println("Do you want the search to be case sensitive? (Y)es or (N)o ");
+		String caseOrNot = caseSensitiveScanner.nextLine().toLowerCase().substring(0, 1);
+		
+		try(Scanner fileReader = new Scanner(scanFile)){
+			int counter = 0;
+		
+				while(fileReader.hasNextLine()) {
+					
+				if(caseOrNot.equals("Y")){
+					counter++;
+					String line = fileReader.nextLine();
+					if(line.contains(wordToSearch)) {
+						System.out.println(counter + ")" +line);
+					} else { 
+						line = fileReader.nextLine();
+						 if(line.contains(wordToSearch)) {
+							 System.out.println(counter + ")" + line);
+						 }
+					
+						} 
+							 
+					
+		
+		
+				} else {
+					counter++;
+					String line = fileReader.nextLine();
+					if(line.toLowerCase().contains(wordToSearch.toLowerCase())) {
+						System.out.println(counter + ")" +line);
+					} else { 
+						line = fileReader.nextLine();
+						 if(line.toLowerCase().contains(wordToSearch.toLowerCase())) {
+							 System.out.println(counter + ")" + line);
+								caseSensitiveScanner.close();
+
+						 }
+					}
+				}
+		
+		
+	
+				}
+		}
+		return "";
 }
 }		
 
