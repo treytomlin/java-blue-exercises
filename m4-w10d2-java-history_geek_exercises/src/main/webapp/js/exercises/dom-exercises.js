a9be77cf2ddc346a9bb461624736ba2a0c26ff38
ttomlin <treytomlin91@gmail.com>
2018-07-25 19:51:10 -0400

    Tuesday Complete

--- m4-w10d2-java-history_geek_exercises/src/main/webapp/js/exercises/dom-exercises.js
+++ m4-w10d2-java-history_geek_exercises/src/main/webapp/js/exercises/dom-exercises.js
@@ -12,35 +12,42 @@ function georgeWashington() {
 
 //Locate all Republican presidents and add the 'red' class.
 function republicanPresidents() {
+    $('.Republican').addClass('red');
 
 }
 
 //Locate all Democrat presidents and add the 'blue' class.
 function democratPresidents() {
+    $('.Democratic').addClass('blue');
 
 }
 
 //Locate all Federalist presidents and add the 'gold' class.
 function otherPresidents() {
+	$('div#presidents > div').not('.Republican').not('.Democratic').addClass('gold');
 
 }
 
 //Locate all presidents named 'James' and add the 'james' class.
 function presidentsNamedJames() {
+	$('[id^="james"]').addClass('james');
 
 }
 
 //Locate each president serving at the beginning of each century and add the 'beginningOfCentury' class.
 function turnOfTheCenturyPresidents() {
+	$('h2').next('div').addClass('beginningOfCentury');
 
 }
 
 //Locate each president serving at the end of the century and add the 'endOfCentury' class.
 function endOfTheCenturyPresidents() {
-    
+	$('div').prev('h2').addClass('endOfCentury');
+
 }
 
 //Locate all presidents serving in the 1900s who appear on currency and add the 'appearOnCurrency' class.
 function currencyPresidents() {
+	$("h2:contains('1900s')").nextAll('.currency').addClass('appearOnCurrency');
 
 }
